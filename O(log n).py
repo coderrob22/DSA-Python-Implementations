fa3189f071d0d960ad350a7c45d11259e68a4118
coderrob22 <robertknott7@gmail.com>
2024-01-16 18:11:47 -0500

    Big O wrapped up

---
+++ O(log n).py
@@ -0,0 +1,9 @@
+# O of log n
+
+# Must use with sorts and the information must already be sorted.
+
+# This is usually called 'Divide and Conquer'
+
+
+# # There is also O(n log n)
+# You see this in sorting algos.
