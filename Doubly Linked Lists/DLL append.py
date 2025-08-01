0380cb98e2c3b13350e8d6738a5db5d406744879
coderrob22 <robertknott7@gmail.com>
2025-08-01 14:40:27 -0400

    doubly linkedlist append

---
+++ Doubly Linked Lists/DLL append.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.previous = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        # If the DLL is empty, set head to the new node. You call always check if its empty by looking at the head.
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
