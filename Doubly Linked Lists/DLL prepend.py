17e929a8630e5f74afa15f2650d0f4a5f4297aa7
coderrob22 <robertknott7@gmail.com>
2025-08-05 12:07:06 -0400

    doubly linkedlist prepend update

---
+++ Doubly Linked Lists/DLL prepend.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
