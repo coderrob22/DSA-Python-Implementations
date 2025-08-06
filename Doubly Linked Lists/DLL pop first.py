e8b43f2660a7188d03690ece2fd124c9e0f08372
coderrob22 <robertknott7@gmail.com>
2025-08-06 10:22:47 -0400

    doubly linkedlist pop first

---
+++ Doubly Linked Lists/DLL pop first.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def pop_first(self, value):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
