bf62be19023eef28b60f13464f2afa691c658c9a
coderrob22 <robertknott7@gmail.com>
2025-08-23 10:49:00 -0400

    Queue dequeue

---
+++ Queue/Queue dequeue.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def dequeue(self):
+        # if the queue has 0 Nodes in it...
+        if self.length == 0:
+            return None
+        # if the queue only has 1 Node in it....
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
