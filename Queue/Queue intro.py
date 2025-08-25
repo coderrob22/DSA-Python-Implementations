db7048ebd6fb42e620241b2d6dc81e00f3cc3d68
coderrob22 <robertknott7@gmail.com>
2025-08-25 11:00:31 -0400

    Queue and stack updates

--- Queue/Queue intro.py
+++ Queue/Queue intro.py
@@ -4,3 +4,5 @@
 # Adding = Enqueue
 # Dequeue from tail = O(1) but enqueue from tail/last is O(n)
 # Enqueue or dequeue from head/first is O(1)
+
+# BOTH adding to and removeing from a Queue is O(1).
---
+++ Stacks/Stacks intro.py
@@ -0,0 +1,2 @@
+# Both pushing and popping from a stack is O(1)
+# It is common to implement a Stack with either a List or LinkedList
