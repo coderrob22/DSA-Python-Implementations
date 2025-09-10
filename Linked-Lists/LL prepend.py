791a350e603ba0b54413e1b3f298cdb9a7ab7729
coderrob22 <robertknott7@gmail.com>
2025-09-10 14:26:50 -0400

    added prepend method for DSA

---
+++ Linked-Lists/LL prepend.py
@@ -0,0 +1,14 @@
+# LinkedList prepend method
+
+def prepend(self, value):
+    new_node = Node(value)
+
+    # If linked list is empty
+    if self.length == 0:
+        self.head = new_node
+        self.tail = new_node
+    else:
+        new_node.next = self.head
+        self.head = new_node
+    self.length += 1
+    return True
