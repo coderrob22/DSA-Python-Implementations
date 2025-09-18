72e76fb62d572a7e6d48e26133bfec4d02dd7977
coderrob22 <robertknott7@gmail.com>
2025-09-18 15:01:16 -0400

    Hash tables intro

---
+++ Hash Tables/Hash table intro.py
@@ -0,0 +1,6 @@
+# 1)Hash tables are one way, meaning you send the key through the hash and it gives you a number (address), but you cannot undo that or send the address to give you the key.
+# 2)Hash tables are deterministic, meaning every time we put the certain key in we expect to get that hash.... deterministic means we know the address
+
+# python includes a built in hash table: dictionaries
+
+# 
