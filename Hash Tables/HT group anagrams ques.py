8b98ccc4eeea6dc8dc1bc9e65dca4b0f43d50181
coderrob22 <robertknott7@gmail.com>
2025-12-12 16:41:32 -0500

    Hash tables leet code ques-- anagrams

---
+++ Hash Tables/HT group anagrams ques.py
@@ -0,0 +1,30 @@
+# HT: Group Anagrams ( ** Interview Question)
+
+# You have been given an array of strings, where each string may contain only lowercase English letters. You need to write a function group_anagrams(strings) that groups the anagrams in the array together using a hash table (dictionary). The function should return a list of lists, where each inner list contains a group of anagrams.
+
+# For example, if the input array is ["eat", "tea", "tan", "ate", "nat", "bat"], the function should return [["eat","tea","ate"],["tan","nat"],["bat"]] because the first three strings are anagrams of each other, the next two strings are anagrams of each other, and the last string has no anagrams in the input array.
+
+# You need to implement the group_anagrams(strings) function and return a list of lists, where each inner list contains a group of anagrams according to the above requirements.
+
+
+def group_anagrams(strings):
+    # create an empty hash table
+    anagram_groups = {}
+ 
+    # iterate through each string in the array
+    for string in strings:
+        # sort each string to get its canonical form
+        # sorted('eat') returns ['a', 'e', 't']
+        # ''.join(['a', 'e', 't']) coverts the array of chars to 'aet' string
+        canonical = ''.join(sorted(string))
+ 
+        # check to see if the canonical form of the string exists in the hash table
+        if canonical in anagram_groups:
+            # if it does then add the string there
+            anagram_groups[canonical].append(string)
+        else:
+            # otherwise create new canonical form and add the string there
+            anagram_groups[canonical] = [string]
+ 
+    # convert the hash table to a list of lists
+    return list(anagram_groups.values())
